c516e7aba55861dfb23288b1932d3e97e0bedfa6
xelz <xelzmm@gmail.com>
2018-10-17 18:57:49 +0800

    add armv7s

--- compile.sh
+++ compile.sh
@@ -13,6 +13,9 @@ clang -mios-version-min=8.0 -isysroot $1 -arch $2 -c $3 -o $4 $5
 compile $SDK_IPHONEOS armv7 test.c test_armv7.o ""
 compile $SDK_IPHONEOS armv7 test.c test_armv7_marker.o -fembed-bitcode-marker
 compile $SDK_IPHONEOS armv7 test.c test_armv7_bitcode.o -fembed-bitcode
+compile $SDK_IPHONEOS armv7s test.c test_armv7s.o ""
+compile $SDK_IPHONEOS armv7s test.c test_armv7s_marker.o -fembed-bitcode-marker
+compile $SDK_IPHONEOS armv7s test.c test_armv7s_bitcode.o -fembed-bitcode
 compile $SDK_IPHONEOS arm64 test.c test_arm64.o ""
 compile $SDK_IPHONEOS arm64 test.c test_arm64_marker.o -fembed-bitcode-marker
 compile $SDK_IPHONEOS arm64 test.c test_arm64_bitcode.o -fembed-bitcode
@@ -20,6 +23,9 @@ compile $SDK_IPHONEOS arm64 test.c test_arm64_bitcode.o -fembed-bitcode
 compile $SDK_IPHONEOS armv7 main.c main_armv7.o ""
 compile $SDK_IPHONEOS armv7 main.c main_armv7_marker.o -fembed-bitcode-marker
 compile $SDK_IPHONEOS armv7 main.c main_armv7_bitcode.o -fembed-bitcode
+compile $SDK_IPHONEOS armv7s main.c main_armv7s.o ""
+compile $SDK_IPHONEOS armv7s main.c main_armv7s_marker.o -fembed-bitcode-marker
+compile $SDK_IPHONEOS armv7s main.c main_armv7s_bitcode.o -fembed-bitcode
 compile $SDK_IPHONEOS arm64 main.c main_arm64.o ""
 compile $SDK_IPHONEOS arm64 main.c main_arm64_marker.o -fembed-bitcode-marker
 compile $SDK_IPHONEOS arm64 main.c main_arm64_bitcode.o -fembed-bitcode
@@ -32,17 +38,20 @@ compile $SDK_IPHONESIMULATOR x86_64 main.c main_x86_64.o ""
 compile $SDK_IPHONEOS armv7 test_armv7.S testS_armv7.o ""
 compile $SDK_IPHONEOS armv7 test_armv7.S testS_armv7_marker.o -fembed-bitcode-marker
 compile $SDK_IPHONEOS armv7 test_armv7.S testS_armv7_bitcode.o -fembed-bitcode
+compile $SDK_IPHONEOS armv7s test_armv7s.S testS_armv7s.o ""
+compile $SDK_IPHONEOS armv7s test_armv7s.S testS_armv7s_marker.o -fembed-bitcode-marker
+compile $SDK_IPHONEOS armv7s test_armv7s.S testS_armv7s_bitcode.o -fembed-bitcode
 compile $SDK_IPHONEOS arm64 test_arm64.S testS_arm64.o ""
 compile $SDK_IPHONEOS arm64 test_arm64.S testS_arm64_marker.o -fembed-bitcode-marker
 compile $SDK_IPHONEOS arm64 test_arm64.S testS_arm64_bitcode.o -fembed-bitcode
 
 for suffix in "" _marker _bitcode; do
-	for arch in armv7 arm64; do
+	for arch in armv7 armv7s arm64; do
 		libtool -static test_${arch}${suffix}.o testS_${arch}${suffix}.o -o libtest_${arch}${suffix}.a
 	done
 done
 
-for arch in armv7 arm64; do
+for arch in armv7 armv7s arm64; do
 	clang -dynamiclib -isysroot $SDK_IPHONEOS -arch ${arch} -mios-version-min=8.0 test_${arch}.o  testS_${arch}.o -o libtest_${arch}.dylib
 	clang -dynamiclib -isysroot $SDK_IPHONEOS -arch ${arch} -mios-version-min=8.0 test_${arch}_marker.o  testS_${arch}_marker.o -o libtest_${arch}_marker.dylib -fembed-bitcode-marker
 	clang -dynamiclib -isysroot $SDK_IPHONEOS -arch ${arch} -mios-version-min=8.0 test_${arch}_bitcode.o  testS_${arch}_bitcode.o -o libtest_${arch}_bitcode.dylib -fembed-bitcode
@@ -56,7 +65,7 @@ done
 for ext in .a .dylib; do
 	lipo -create libtest_i386${ext} libtest_x86_64${ext} -output libtest_iphonesimulator${ext}
 	for suffix in "" _marker _bitcode; do
-		lipo -create libtest_armv7${suffix}${ext} libtest_arm64${suffix}${ext} -output libtest_iphoneos${suffix}${ext}
-		lipo -create libtest_armv7${suffix}${ext} libtest_arm64${suffix}${ext} libtest_i386${ext} libtest_x86_64${ext} -output libtest_all${suffix}${ext}
+		lipo -create libtest_armv7${suffix}${ext} libtest_armv7s${suffix}${ext} libtest_arm64${suffix}${ext} -output libtest_iphoneos${suffix}${ext}
+		lipo -create libtest_armv7${suffix}${ext} libtest_armv7s${suffix}${ext} libtest_arm64${suffix}${ext} libtest_i386${ext} libtest_x86_64${ext} -output libtest_all${suffix}${ext}
 	done
 done
